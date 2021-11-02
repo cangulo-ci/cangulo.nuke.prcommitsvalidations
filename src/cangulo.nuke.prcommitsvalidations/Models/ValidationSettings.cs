d67c04906f4e8cac29730bafe249811f447c8a1d
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2021-11-02 09:18:17 +0100

    fix: created initial version, depends that a PR is created or updated, it can't be executed on a branch without a PR associated

---
+++ src/cangulo.nuke.prcommitsvalidations/Models/ValidationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace cangulo.nuke.prcommitsvalidations.Models
+{
+    public class ValidationSettings
+    {
+        public string[] ConventionalCommitTypes { get; set; }
+        public string CommitMsgRegexValidator { get; set; }
+        public bool OutputCommitList { get; set; }
+        public string OutputCommitListPath { get; set; } = "commits.txt";
+    }
+}
